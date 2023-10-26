96e30939d30840f346b8e9f7ed6107eb78e0b26a
natalia luna <natyluna92@gmail.com>
2023-10-25 21:49:50 -0300

    creacion header

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 5000;
 
 //Definimos dónde buscamos archivos js, css e imagenes
-app.use("", express.static(`${__dirname}/public`));
+app.use("", express.static(`${__dirname}/publics`));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
