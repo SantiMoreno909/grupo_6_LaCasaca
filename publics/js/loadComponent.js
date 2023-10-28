eeb1f8a6642da7c33fc28fb304a2897d68c4c305
natalia luna <natyluna92@gmail.com>
2023-10-28 20:30:17 -0300

    organizacion de rutas

---
+++ publics/js/loadComponent.js
@@ -0,0 +1,11 @@
+function loadHTML(url, elementId) {
+    fetch(url) //busca el archivo
+        .then(response => response.text()) //obtiente el html
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data; //devuelve lo q tenga en el html q se lo llame
+        });
+}
+
+loadHTML("/components/header.html", "header");
+
+
