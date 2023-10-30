640890d15e02ff2409f222eaaad8ae3bb9ed5561
Santi Moreno <santiagom.contact@gmail.com>
2023-10-30 17:23:17 -0300

    Avances con cards

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 //Definimos dónde buscamos archivos js, css e imagenes
 app.use("", express.static(`${__dirname}/publics`));
-
 
 //definimos las distintas rutas
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get('/login', (req,res)=>{
-  res.sendFile(__dirname + '/views/login.html');
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
 });
 
-app.get('/register', (req,res)=>{
-  res.sendFile(__dirname + '/views/register.html');
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
 });
 
-app.get('/detalle', (req,res)=>{
-  res.sendFile(__dirname + '/views/productDetail.html');
+app.get("/detalle", (req, res) => {
+  res.sendFile(__dirname + "/views/productDetail.html");
 });
 
 app.listen(PORT, () => console.log("Server funcionando en puerto " + PORT));
