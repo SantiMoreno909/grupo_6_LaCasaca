5ac788446380254712f162fee1c38410d6356d5c
Santi Moreno <santiagom.contact@gmail.com>
2023-11-23 20:36:11 -0300

    Pagina de productos y actualización carruseles de busqueda

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,15 +1,15 @@
-
 const express = require("express");
 const app = express();
 
 const productsController = require("../controllers/productsController");
 
-let router= express.Router();
+let router = express.Router();
 //definimos las distintas rutas
-router.get('/',productsController.index)
-router.get('/login',productsController.login)
-router.get('/carrito',productsController.carrito)
-router.get('/detail',productsController.detail)
-router.get('/register',productsController.register)
+router.get("/", productsController.index);
+router.get("/login", productsController.login);
+router.get("/carrito", productsController.carrito);
+router.get("/detail", productsController.detail);
+router.get("/register", productsController.register);
+router.get("/:clubNombre", productsController.clubes);
 
 module.exports = router;
