af7b4b48485dd513be3947909b74287ba10f0399
natalia luna <natyluna92@gmail.com>
2024-01-10 20:34:31 -0300

    base de datos

---
+++ data/data.sql
@@ -0,0 +1,23 @@
+insert lacasaca.ligas (nombre,pais) values ("Liga Argentina", "Argentina");
+insert lacasaca.ligas (nombre,pais) values ("Copa Argentina", "Argentina");
+insert lacasaca.ligas (nombre,pais) values ("Super Argentina", "Argentina");
+
+insert lacasaca.marcas (nombre) value ("Adidas");
+insert lacasaca.marcas (nombre) value ("Nike");
+insert lacasaca.marcas (nombre) value ("Under Armour");
+
+insert lacasaca.equipos (nombre,id_liga,id_marca) value ("Boca Junior", 1,1);
+insert lacasaca.equipos (nombre,id_liga,id_marca) value ("River Plate", 1,1);
+insert lacasaca.equipos (nombre,id_liga,id_marca) value ("Estudiantes", 2,3);
+
+insert lacasaca.productos (nombre,precio,talle,descripcion,id_equipo,id_liga,stock,id_marca)
+value ("Camiseta Boca Original 2023", 25000,"S","original",1,1,1,1);
+
+insert lacasaca.productos (nombre,precio,talle,descripcion,id_equipo,id_liga,stock,id_marca)
+value ("Camiseta River Original 2023", 20000,"M","original",2,1,1,1);
+
+insert lacasaca.usuarios (nombre,apellido,email,telefono,genero,direccion_envios,contrasenia,confirmar_contrasenia)
+values ("Natalia","Luna","natyluna92@gmail.com",156118899,"femenino","Ocampo 1300","1234","1234")
+
+insert lacasaca.usuarios (nombre,apellido,email,telefono,genero,direccion_envios,contrasenia,confirmar_contrasenia)
+values ("Santiago","Moreno","santiago@gmail.com",15437049,"masculino","Otro 123","1234","1234")
